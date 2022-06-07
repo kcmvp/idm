a4169c04b6cfdba2ccef154541ddf6ad8e3b06a0
kcmvp <kcheng.mvp@gmail.com>
2022-06-07 16:28:30 +0800

    add gin support

---
+++ start.go
@@ -0,0 +1,30 @@
+package iam
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/kcmvp/iam.go/ent"
+	"log"
+	"net/http"
+)
+
+func main() {
+	client, err := ent.Open("mysql", "<user>:<pass>@tcp(<host>:<port>)/<database>?parseTime=True")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
